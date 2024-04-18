ef308e659942520914bc85788e3f5edd41fecdec
Yayuan Zhu <yayuanzhu_@hotmail.com>
2024-04-18 15:53:32 +0200

    Implemented emptyDir()

--- functions/functions_bash.sh
+++ functions/functions_bash.sh
@@ -1,11 +1,29 @@
 #Creates a directory if it does not exist yet.
-createDir() {
+# createDir() {
+#   local dirPath="$1"
+#   
+#   if [ ! -d "$dirPath" ]; then
+#     mkdir -p "$dirPath"
+#     echo "Directory created: $dirPath"
+#   else
+#     echo "Directory already exists: $dirPath"
+#   fi
+# }
+# 
+
+emptyDir() {
   local dirPath="$1"
+  local i="$2"
   
-  if [ ! -d "$dirPath" ]; then
-    mkdir -p "$dirPath"
-    echo "Directory created: $dirPath"
+  if [ -d "$dirPath" ]; then
+    if [ "$(ls -A "$dirPath")" ]; then
+      echo "Directory is not empty, clearing contents..."
+      rm -r "$dirPath"/*
+      echo "Directory emptied ($i): $dirPath"
+    else
+      echo "$dirPath is empty ($i)"
+    fi
   else
-    echo "Directory already exists: $dirPath"
+    echo "Directory $dirPath not found ($i)"
   fi
 }
