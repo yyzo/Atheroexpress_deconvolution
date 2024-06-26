f350f0193a477d1e7c801d2256393a32d1263dd9
Yayuan Zhu <yayuanzhu_@hotmail.com>
2024-06-26 13:43:24 +0200

    Uncomment createDir()

--- functions/functions_bash.sh
+++ functions/functions_bash.sh
@@ -1,15 +1,15 @@
 #Creates a directory if it does not exist yet.
-# createDir() {
-#   local dirPath="$1"
-#   
-#   if [ ! -d "$dirPath" ]; then
-#     mkdir -p "$dirPath"
-#     echo "Directory created: $dirPath"
-#   else
-#     echo "Directory already exists: $dirPath"
-#   fi
-# }
-# 
+createDir() {
+  local dirPath="$1"
+
+  if [ ! -d "$dirPath" ]; then
+    mkdir -p "$dirPath"
+    echo "Directory created: $dirPath"
+  else
+    echo "Directory already exists: $dirPath"
+  fi
+}
+
 
 emptyDir() {
   local dirPath="$1"
