70668dc8719a9f3d39bcfbefbbb67ee1fbb9b250
Yayuan Zhu <yayuanzhu_@hotmail.com>
2024-06-27 13:22:00 +0200

    Replace ownMinimalTheme with ownTheme

--- functions/functions_R.R
+++ functions/functions_R.R
@@ -204,29 +204,12 @@ dunnTestCustom <- function(data) {
   
 }
 
-ownMinimalTheme <- function() {
-  theme(
-    plot.title = element_text(face = "bold", size = 18),
-    #Modify x- and y-axes
-    axis.title.x = element_text(size = 16, margin = margin(t = 5)),
-    axis.title.y = element_text(size = 16, margin = margin(r = 10)),
-    axis.text.x = element_text(
-      angle = 60,
-      vjust = 1,
-      hjust = 1,
-      size = 13
-    ),
-    axis.text.y = element_text(size = 13),
-    axis.ticks.y = element_blank(),
-    #Modify panel
-    panel.background = element_blank(),
-    panel.grid.major.y = element_line(color = "lightgray", linewidth = 0.8),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.y = element_line(color = "lightgray", linewidth = 0.1),
-    panel.grid.minor.x = element_blank(),
-    #Modify legend
-    legend.key.size = unit(1, "cm"),
-    legend.title = element_text(size = 16),
-    legend.text = element_text(size = 13)
+ownTheme <- function() {
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title.x = element_text(size = 13, hjust = 0.5, margin = margin(t = 15)),
+        axis.title.y = element_text(size = 13, hjust = 0.5, margin = margin(r = 15)),
+        panel.grid.minor.y = element_line(color = "gray", linewidth = 0.5),
+        legend.title = element_text(size = 13),
+        legend.text = element_text(size = 11)
   )
 }
